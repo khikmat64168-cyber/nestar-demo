56e12ce5e992e4818441c2504c5a208dc6a8a60f
matt <khikmat64168@gmail.com>
2026-09-12 13:51:18 +0900

    feat: organize and test pages router sytem

---
+++ pages/api/community/index.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const Community = () => {
+  const [title, setTitle] = useState<string>('hello');
+  return <div> COMMUNITY</div>;
+};
+
+export default Community;
