df0784a80460ed9868dd0ddca0a75186d73cfc69
matt <khikmat64168@gmail.com>
2026-09-15 03:15:41 +0900

    feat: develop apollo client connection

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,14 +1,15 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 // No Subscription required for develop process
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3007/graphql",
+  uri: 'http://localhost:3008/graphql',
 });
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
+  connectToDevTools: process.env.NODE_ENV === 'development',
 });
 
 export default client;
--- libs/components/layout/LayoutHome.tsx
+++ libs/components/layout/LayoutHome.tsx
@@ -1,33 +1,33 @@
-import { Stack } from "@mui/material";
-import Head from "next/head";
-import Top from "../Top";
-import Footer from "../Footer";
-import HeaderFilter from "../homepage/HeaderFilter";
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import useDeviceDetect from "@/libs/hooks/useDeviceDetect";
+import { Stack } from '@mui/material';
+import Head from 'next/head';
+import Top from '../Top';
+import Footer from '../Footer';
+import HeaderFilter from '../homepage/HeaderFilter'; //@ts-ignore
+import 'swiper/css'; //@ts-ignore
+import 'swiper/css/pagination'; //@ts-ignore
+import 'swiper/css/navigation';
+import useDeviceDetect from '@/libs/hooks/useDeviceDetect';
 
 const withLayoutMain = (Component: any) => {
   return (props: any) => {
     const device = useDeviceDetect();
 
-    if (device == "mobile") {
+    if (device == 'mobile') {
       return (
         <>
           <Head>
             <title>Nestar</title>
           </Head>
           <Stack id="mobile-wrap">
-            <Stack id={"top"}>
+            <Stack id={'top'}>
               <Top />
             </Stack>
 
-            <Stack id={"main"}>
+            <Stack id={'main'}>
               <Component {...props} />
             </Stack>
 
-            <Stack id={"footer"}>
+            <Stack id={'footer'}>
               <Footer />
             </Stack>
           </Stack>
@@ -40,20 +40,20 @@ const withLayoutMain = (Component: any) => {
             <title>Nestar</title>
           </Head>
           <Stack id="pc-wrap">
-            <Stack id={"top"}>
+            <Stack id={'top'}>
               <Top />
             </Stack>
-            <Stack className={"header-main"}>
-              <Stack className={"container"}>
+            <Stack className={'header-main'}>
+              <Stack className={'container'}>
                 <HeaderFilter />
               </Stack>
             </Stack>
 
-            <Stack id={"main"}>
+            <Stack id={'main'}>
               <Component {...props} />
             </Stack>
 
-            <Stack id={"footer"}>
+            <Stack id={'footer'}>
               <Footer />
             </Stack>
           </Stack>
